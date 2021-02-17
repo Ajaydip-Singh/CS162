9981e632c68d108ea2b18970ec0fdd78e535e8f2
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-17 23:34:24 +0300

    add alternative way of getting data

--- session11/crypto.py
+++ session11/crypto.py
@@ -8,8 +8,22 @@ def get_latest_trading_prices(currencies):
         query = {'command': 'returnTradeHistory', 'currencyPair': currency}
 
         r = requests.get('https://poloniex.com/public', params=query)
+
+        # raise an exception in case there was an error
         r.raise_for_status()
 
         print(f"{currency} : {r.json()[0]['rate']}")
 
-get_latest_trading_prices(['BTC_ETH', 'BTC_ETC'])    
+# get_latest_trading_prices(['BTC_ETH', 'BTC_ETC'])    
+
+def get_latest_trading_prices():
+
+    r = requests.get('https://poloniex.com/public?command=returnTicker')
+
+    # raise an exception in case there was an error
+    r.raise_for_status()
+
+    for currency in r.json():
+        print(f"{currency} : {r.json()[currency]['last']}")
+
+get_latest_trading_prices()
